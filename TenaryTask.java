f256e77d53142bcd0d85a4539e9aeacad187035c
ndayingiye <ndayiringiyedavid120@gmail.com>
2025-07-08 15:44:29 +0200

    feat:/ alread number checked are if positive or negative

---
+++ TenaryTask.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class TenaryTask {
+
+    public static void main(String[] args) {
+        Scanner inputScanner = new Scanner(System.in);
+        int num;
+        String result;
+        System.out.print("Enter a number:");
+        num = inputScanner.nextInt();
+
+        result = (num > 0 && num < 0) ? "positive" : "negative";
+        System.out.println("the a number is :" + result);
+        inputScanner.close();
+    }
+}
