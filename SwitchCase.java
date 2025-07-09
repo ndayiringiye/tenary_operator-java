918044410f973c2763162029309368ef084497db
ndayingiye <ndayiringiyedavid120@gmail.com>
2025-07-09 09:43:34 +0200

    feat:/on switch casing issue fixed successfully

--- SwitchCase.java
+++ SwitchCase.java
@@ -1,4 +1,3 @@
-
 import java.util.Scanner;
 
 public class SwitchCase {
@@ -7,34 +6,37 @@ public class SwitchCase {
         Scanner inputScanner = new Scanner(System.in);
         int day;
         String dayName;
-        System.out.print("Enter a number between 1 and  7:");
+
+        System.out.print("Enter a number between 1 and 7: ");
         day = inputScanner.nextInt();
 
         switch (day) {
             case 1:
-                dayName = "monday";
+                dayName = "Monday";
                 break;
             case 2:
-                dayName = "teusday";
+                dayName = "Tuesday";
                 break;
             case 3:
-                dayName = "wenesday";
+                dayName = "Wednesday";
                 break;
             case 4:
-                dayName = "thusday";
+                dayName = "Thursday";
                 break;
             case 5:
-                dayName = "friday";
+                dayName = "Friday";
                 break;
             case 6:
-                dayName = "saturday";
+                dayName = "Saturday";
                 break;
             case 7:
-                dayName = "sunday";
-        defualt:
-        dayName = "invalid day";
-        };
-        System.out.println("the day is " + dayName);
+                dayName = "Sunday";
+                break;
+            default:
+                dayName = "Invalid day";
+        }
+
+        System.out.println("The day is: " + dayName);
         inputScanner.close();
     }
 }
